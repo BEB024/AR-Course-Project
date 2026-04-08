be4a35cf3a1cce2a2a53c7346fc4808a4de279be
BEB024 <ilabib2004@gmail.com>
2026-04-08 13:18:45 +0200

    AR Lab 2
    Single image tracking.

---
+++ Assets/Scripts/CustomARTrackedImageManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CustomARTrackedImageManager : MonoBehaviour
+{
+    [SerializeField] private ARTrackedImageManager imageManager;
+
+    void OnEnable()
+    {
+        imageManager.trackablesChanged.AddListener(OnChanged);
+    }
+
+    void OnDisable()
+    {
+        imageManager.trackablesChanged.RemoveListener(OnChanged);
+    }
+
+    void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        // Handle added images
+        foreach (var newImage in eventArgs.added)
+        {
+            // Handle added event
+        }
+
+        // Handle updated images
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            // Handle updated event
+        }
+
+        // Handle removed images
+        foreach (var removed in eventArgs.removed)
+        {
+            TrackableId removedImageTrackableId = removed.Key;
+            ARTrackedImage removedImage = removed.Value;
+
+            // Handle removed event
+        }
+    }
+}
